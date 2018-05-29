8376e76e5eabb65b864e4118eace81431580f775
Tnks <robsons@alunos.utfpr.edu.br>
2018-05-29 19:24:13 -0300

    incluido view show e index de sessao count de pautas

--- config/application.rb
+++ config/application.rb
@@ -9,18 +9,10 @@ Bundler.require(*Rails.groups)
 module ProjObsSocial
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-<<<<<<< HEAD
-    config.time_zone = 'Brasilia'
- 
-   # config.i18n.enforce_available_locales = false
-    #config.i18n.available_locales = ["pt-BR"]
-    config.i18n.default_locale = "pt-BR"
-=======
     config.time_zone = "Brasilia"
 
-    config.i18n.enforce_available_locales = false
-    config.i18n.available_locales = ["pt-BR"]
-    config.i18n.default_locale = :'pt-BR'
->>>>>>> 46302edc735467f743d5f55a09072a761777cf70
+    # config.i18n.enforce_available_locales = false
+    #config.i18n.available_locales = ["pt-BR"]
+    config.i18n.default_locale = "pt-BR"
   end
 end
