4669610ecd9c735ee9e48bc5d17f18af738330a8
Tnks <robsons@alunos.utfpr.edu.br>
2018-05-29 21:16:17 -0300

    alteacoes de layoute para sessao, vereador e projeto, criado modal para inserção de novo tipo de projeto

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,7 @@
 class Session < ApplicationRecord
   has_many :projects
+
+  def formatted_date
+    date.to_time.strftime("%d/%m/%Y")
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  Councilman.create([{
+    name: Faker::Name.name,
+    nickname: Faker::GameOfThrones.character,
+    political_party: Faker::Name.initials,
+    political_position: 0,
+  }])
+end
