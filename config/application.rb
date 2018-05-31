412e2e046b6ef19eab0b6eb8334e932609d2fb53
Valdinei Moraes <valdineimoraes@alunos.utfpr.edu.br>
2018-05-31 15:42:31 -0300

    i18n mudanças

--- config/application.rb
+++ config/application.rb
@@ -9,18 +9,13 @@ Bundler.require(*Rails.groups)
 module ProjObsSocial
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-<<<<<<< HEAD
-    config.time_zone = 'Brasilia'
- 
-   # config.i18n.enforce_available_locales = false
-    #config.i18n.available_locales = ["pt-BR"]
-    config.i18n.default_locale = "pt-BR"
-=======
-    config.time_zone = "Brasilia"
 
-    config.i18n.enforce_available_locales = false
-    config.i18n.available_locales = ["pt-BR"]
-    config.i18n.default_locale = :'pt-BR'
->>>>>>> 46302edc735467f743d5f55a09072a761777cf70
+    config.time_zone = 'Brasilia'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = [:en, :"pt-BR"]
+    config.i18n.default_locale = :"pt-BR"
+
   end
 end
