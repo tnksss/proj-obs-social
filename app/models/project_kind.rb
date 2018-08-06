c586144b7c433ed3a229a3122993111e65c78b11
tnksss <robsons@alunos.utfpr.edu.br>
2018-08-06 11:53:50 -0300

    add dependent destroy tipo de projeto

--- app/models/project_kind.rb
+++ app/models/project_kind.rb
@@ -3,5 +3,6 @@ class ProjectKind < ApplicationRecord
     scope :search, -> (query) { where("kind like ?", "%#{query}%")}
 
     validates :kind, presence: true, uniqueness: true
+    has_many :projects, dependent: :destroy
     
 end
